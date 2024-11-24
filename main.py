2ca94aff538284d27d5ad9f092a0b2cd5a30ea16
ValeriyV-codex <96054135+ValeriyV-codex@users.noreply.github.com>
2024-11-24 23:05:45 +0300

    port

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import os
+import uvicorn
+from fastapi import FastAPI
 from typing import Optional
 
-from fastapi import FastAPI
-
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
@@ -12,3 +12,8 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    # Используем порт из переменной окружения $PORT, дефолт 8080
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
